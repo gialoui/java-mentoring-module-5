c44db2a0c986e0b2fe5743b85fa2cc91bc54e59d
KhangNDD <khang_nguyen1@epam.com>
2022-11-29 14:24:13 +0700

    Init project

---
+++ src/test/java/com/java/mentoring/module5/Module5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.mentoring.module5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
