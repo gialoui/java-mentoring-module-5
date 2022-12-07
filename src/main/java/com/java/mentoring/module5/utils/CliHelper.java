068917199646a48bd39583ab6f923ebc1f5205d4
KhangNDD <khang_nguyen1@epam.com>
2022-12-07 11:15:52 +0700

    feat: Implement read params function

--- src/main/java/com/java/mentoring/module5/utils/CliHelper.java
+++ src/main/java/com/java/mentoring/module5/utils/CliHelper.java
@@ -4,10 +4,12 @@ import com.java.mentoring.module5.enums.MessengerMode;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * @author khangndd
@@ -33,6 +35,24 @@ public class CliHelper {
     }
 
     public Map<String, String> readParamsFromConsole(InputStream inputStream) {
-        return new HashMap<>();
+        try (Scanner scanner = new Scanner(inputStream, String.valueOf(StandardCharsets.UTF_8))) {
+            System.out.format("Enter parameters for template in format parameterName=parameterValue%n"
+                    + "end entering parameters with new line %n");
+            String parameterLine;
+            var params = new HashMap<String, String>();
+            while (scanner.hasNextLine()) {
+                parameterLine = scanner.nextLine();
+
+                // Stop receiving if the next line is empty
+                if (parameterLine.isEmpty()) {
+                    break;
+                }
+
+                final String[] parameterParts = parameterLine.split("=", 2);
+                params.put(parameterParts[0], parameterParts[1]);
+            }
+
+            return params;
+        }
     }
 }
